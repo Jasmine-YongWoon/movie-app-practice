5466b8219ee21aa9f5ec4726401f290227759eb6
Jasmine-YongWoon <hanyw6401@gmail.com>
2023-01-24 20:00:07 +0900

    09-상태관리

--- src/core/heropy.js
+++ src/core/heropy.js
@@ -16,7 +16,6 @@ export class Component {
   }
 }
 
-
 // Router //
 function routeRender(routes) {
   if (!location.hash) {
@@ -52,5 +51,29 @@ export function createRouter(routes) {
       routeRender(routes)
     })
     routeRender(routes)
+  }
+}
+
+// Store //
+export class Store {
+  constructor(state) {
+    this.state = {}
+    this.observers = {}
+    for (const key in state) {
+      Object.defineProperty(this.state, key, {
+        get: () => state[key], // state['message']
+        set: val => {
+          state[key] = val
+          // this.observers['message']()
+          this.observers[key].forEach(observer => observer(val))
+        }
+      })
+    }
+  }
+  subscribe(key, cb) {
+    // { message: [cb1, cb2, cb3, ...]}
+    Array.isArray(this.observers[key])
+      ? this.observers[key].push(cb)
+      : this.observers[key] = [cb]
   }
 }
